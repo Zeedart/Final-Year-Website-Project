6baf85fcbdce1ca949f05b8f5e8e6c8a3fe2a7a3
Yazeed Alkalmi <115902643+Zeedart@users.noreply.github.com>
2025-05-15 20:59:57 +0200

    Added a ShowAll and Hide functionality to the News Feed

--- HomepageSection/FacebookFeed.jsx
+++ HomepageSection/FacebookFeed.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 function FacebookNewsFeed() {
     const [posts, setPosts] = useState([]);
+    const [showAll, setShowAll] = useState(false);
 
     useEffect(() => {
         const feedUrl = "https://rss.app/feeds/v1.1/HuaNOdhosEH35AbD.json"; // Your JSON feed URL
@@ -11,11 +12,15 @@ function FacebookNewsFeed() {
             .catch((err) => console.error("Failed to load feed", err));
     }, []);
 
+    function HandleShow(){
+        setShowAll(prevShow => !prevShow)
+    }
+
     return (
         <>
         <p className="News-header">Latest News</p>
         <div className="news-feed-container">
-            {posts.slice(0, 3).map((post, index) => (
+            {showAll? posts.map((post, index) => (
                 <div
                     key={index}
                     className="feed"
@@ -39,8 +44,35 @@ function FacebookNewsFeed() {
                         </a>
                     </div>
                 </div>
-            ))}
+            )): 
+            posts.slice(0, 3).map((post, index) => (
+                <div
+                    key={index}
+                    className="feed"
+                >
+                    <img
+                        src={post.image || "https://via.placeholder.com/400x225"} // Use the post's thumbnail or a placeholder
+                        alt="Thumbnail"
+                        className="w-full h-48 object-cover"
+                    />
+                    <div className="below-pic">
+                        <h2 className="feed-title">{post.title.slice(0, 90)}...</h2>
+                        <p className="feed-description">
+                            {post.content_text.slice(0, 150)}...
+                        </p>
+                        <p className="feed-date">{post.date_published.slice(0, 10)}</p>
+                        <a
+                            href={post.url}
+                            target="_blank"
+                            className="link-tag-btn"
+                        ><button className="read-more-btn">Read More </button>
+                        </a>
+                    </div>
+                </div>
+            ))
+            }
         </div>
+        <button className="Show-btn" onClick={HandleShow}>{showAll? "Hide" : "Show All"}</button>
     </>
     );
 }
